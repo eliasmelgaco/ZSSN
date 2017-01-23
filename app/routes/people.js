ad29d37c4253d8850642f75ba22790642fcf2711
Elias Melgaço <eliasmelgaco@MacBook-Pro-de-Elias.local>
2017-01-22 22:45:12 -0200

    j

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,20 +1,5 @@
 import Ember from 'ember';
 
-/*var Person = Ember.Object.extend({
-    name: '',
-    age: 0,
-    gender: '',
-    lonlat: '',
-    items: '' //Inventory Items in the format 'Water:10;Food:5'
-});*/
-//{
-//"id": "bf6fc093-2093-4819-be1e-216f210fb42c",
-//"name":"TheFox",
-//"age":23,
-//"gender":"M",
-//"lonlat":POINT(-5.775445333750184 -35.25417849774169),
-//"created_at":"2017-01-21T17:01:31.204Z",
-//"updated_at":"2017-01-21T17:01:31.204Z"
-//}
 export default Ember.Route.extend({
+    
 });
